2d8a37c603a30cb57765f1a08b48bcbcd0c23d88
Kelvin Gluszszak Lopes <kelvin_gl@hotmail.com>
2017-10-24 23:04:44 -0200

    Implementação inicial

--- example.R
+++ example.R
@@ -4,12 +4,29 @@ install.packages("OpenStreetMap")
 # Carrega pacote
 library(OpenStreetMap)
 
-# Gera um mapa simples
+# Gera um mapa mundial simples
 map <- openmap(
-  upperLeft = c(8.23,-76.73), 
-  lowerRight = c(-34.45,-31.82)
+  upperLeft  = c(85.0, -180.0), 
+  lowerRight = c(-85.0, 180.0),
+  type = "osm"
 )
 
-# Plota mapa
-plot(map)
+# Cria projeção do mapa, para plot com latitude e longitude
+mapLatLon = openproj(map)
 
+# Plota projeção
+plot(mapLatLon)
+
+# Dataset : http://datadrivensecurity.info/blog/pages/dds-dataset-collection.html
+data <- read.delim(
+  file = "marx-geo.csv",
+  sep = ","
+)
+
+# Gera "pontinhos"
+points(
+  x = data$longitude, 
+  y = data$latitude, 
+  col = "red",
+  cex=.1
+)
